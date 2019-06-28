06986609a7e109a76fc7e9258e79691b2d026d15
RajenderDandyal <rajender0184@gmail.com>
2019-06-28 13:48:51 +0530

    objects

--- 1/annotations/objects.ts
+++ 1/annotations/objects.ts
@@ -1,5 +1,5 @@
 const profile = {
-  name: "Rajender",
+  Fname: "Rajender",
   age: 16,
   coords: {
     lat: 0,
@@ -9,3 +9,10 @@ const profile = {
     this.age = age;
   }
 };
+
+const { age, Fname }: { age: number; Fname: string } = profile;
+const {
+  coords: { lat, lng }
+}: { coords: { lat: number; lng: number } } = profile;
+
+console.log(Fname, age, lat, lng);
